1cb213cd131df4b72d87c9d663c7944ad9de7337
Aleksandr <alekspervykh2230521@gmail.com>
2026-09-14 16:30:23 +0300

    feat: replace app to separate file

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,6 @@
-import express, { Request, Response } from 'express';
+import app from "./app.js";
 
-const app = express();
 const PORT = process.env.PORT || 3000;
-
-app.use(express.json());
-
-app.get('/', (req: Request, res: Response) => {
-  res.json({ message: 'API успешно работает на TypeScript!' });
-});
 
 app.listen(PORT, () => {
   console.log(`[server]: Сервер запущен на http://localhost:${PORT}`);
