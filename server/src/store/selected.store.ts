d59a7df20d910b5135aa26dd34dba38e46e2dbff
Aleksandr <alekspervykh2230521@gmail.com>
2026-09-14 18:45:00 +0300

    feat: get selected from store

--- server/src/store/selected.store.ts
+++ server/src/store/selected.store.ts
@@ -1,6 +1,8 @@
+import { Item } from "../types/types.js";
+
 type Selected = {
   set: Set<string>;
-  list: string[];
+  list: Item[];
 };
 
 const selected: Selected = {
@@ -12,14 +14,35 @@ export function getSelectedSet() {
   return selected.set;
 }
 
-export function getSelectedItems() {
-  return selected.list;
-}
-
 export function addToSelected(id: string) {
   if (selected.set.has(id)) {
     return;
   }
   selected.set.add(id);
   selected.list.push(id);
+}
+
+export function getSelectedItemsFromStore({
+  page = 1,
+  limit = 20,
+  search = "",
+}) {
+  const searchStr = search.trim();
+
+  if (!searchStr) {
+    const offset = (page - 1) * limit;
+    const items = selected.list.slice(offset, offset + limit);
+
+    return {
+      items,
+    };
+  }
+
+  const matchedIds = selected.list.filter(({ id }) => id.includes(searchStr));
+  const offset = (page - 1) * limit;
+  const items = matchedIds.slice(offset, offset + limit);
+
+  return {
+    items,
+  };
 }
